5995d9db5dfb2bd0a4b3d2f819fa3ce938b53789
Konstantin Lepeshenkov <kolepes@microsoft.com>
2020-11-12 12:28:49 +0100

    minor fixes

--- api/AppointmentEntity/index.ts
+++
@@ -1,5 +0,0 @@
-import * as df from "durable-functions"
-import { AppointmentEntity, AppointmentState } from "./AppointmentEntity";
-
-// Boilerplate to expose AppointmentEntity as a Durable Entity
-export default df.entity((context) => new AppointmentEntity(context, () => new AppointmentState()).handleSignal());
